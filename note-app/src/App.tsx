05d0b3cd9832e77f5bf117a1b3c5b6a014913d27
Fayez Morsel <fayezmorsel77@gmail.com>
2025-09-05 15:09:00 +0300

    react and tailwind

---
+++ note-app/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import NotesContainer from './components/NotesContainer'
+
+function App () {
+  return (
+    <div className='min-h-screen bg-blue-950'>
+      <NotesContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ note-app/src/components/NotesContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NotesContainer() {
+  return (
+    <div className='md:w-[80%] w-[100%]  mx-auto p-8'>
+        <h1 className='text-3xl font-bold text-white'>Notes App</h1>
+    </div>
+  )
+}
